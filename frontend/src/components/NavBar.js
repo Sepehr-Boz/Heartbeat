27d20e414032da82d8e71960e124123ec96993d9
Sepehr <51531251+Sepehr-Boz@users.noreply.github.com>
2026-01-27 21:46:29 +0000

    Created Navbar component. To use the component import it and add it your page, it will automatically stick to the bottom and navigate between home, qr, and profile on any of the icon pages. The NavBar component also takes in 3 parameters/properties isHome, isQR, isProfile which are true/false booleans to determine which icon to set black and which to set grey (the current page should be the black icon).

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,10 +1,42 @@
-import { Component } from "react";
+import { useNavigate } from "react-router-dom";
 
+import barsIcon from "./ChartIcon.svg";
+import qrIcon from "./QRIcon.svg";
+import profIcon from "./PersonIcon.svg";
 
-class NavBar extends Component{
-    render(){ return(
-        <div></div>
-    )};
+import "./NavBar.css";
+
+/**
+ * Creates a custom clickable icon for the navbar
+**/
+function NavIcon(img, alt="icon", path, isCurrent){
+    let navigate = useNavigate();
+
+    return (
+        <div className="navicon"
+        onClick={() => {navigate(path)}}>
+            <img src={img} alt={alt} id={isCurrent ? "current" : "other"}></img>
+        </div>
+    )
+}
+
+/**
+ * Creates the navbar at the bottom of the page
+ * props has 3 variables: isHome, isQR, isProfile to determine which of
+ * the 3 icons to make black and which to make grey
+**/
+function NavBar(props){
+    const homebtn = NavIcon(barsIcon, "home icon", "/home", props.isHome);
+    const qrbtn = NavIcon(qrIcon, "qr icon", "/qr", props.isQR);
+    const profbtn = NavIcon(profIcon, "profile icon", "/profile", props.isProfile);
+
+    return (
+        <div className="navbar">
+            {homebtn}
+            {qrbtn}
+            {profbtn}
+        </div>
+    );
 }
 
 export default NavBar;
