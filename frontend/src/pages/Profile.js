b9eab72baac05c5dcae9745c7a04010b8ab977b4
AidenComiskey <aiden.whisk@hotmail.com>
2026-01-28 15:57:17 +0000

    initial template for profile page written up

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -3,9 +3,29 @@ import { Component } from "react";
 
 
 class ProfilePage extends Component{
-    render(){ return (
-        <div></div>
-    )};
+    render() {
+        // these are just placeholders to be changed depending on users profile pic and name
+        const username = "Test";
+        const profileImage = "Image.jpg";
+
+        return (
+        <div className="Profile">
+            <div className="profile-header">
+                <img src={profileImage} alt="Profile" className="profile-pic"/>
+                <h2 className="username">{username}</h2>
+            </div>
+            <div className="options-bar">
+                <span>------ Options ------</span>
+            </div>
+            <div className="profile-options">
+                <button className="option-button">Settings</button>
+                <button className="option-button">Scan User Code</button>
+                <button className="option-button">Contact GP</button>
+                <button className="option-button logout-button">Logout</button>
+            </div>
+        </div>
+        );
+    }
 }
 
 export default ProfilePage;
