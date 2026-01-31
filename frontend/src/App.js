ba80392fc060e31cc7bd5765a9e58ea09844b139
Sepehr <51531251+Sepehr-Boz@users.noreply.github.com>
2026-01-31 19:34:21 +0000

    Fixed small issue with WelcomePage css, other css works fine, added back routes to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,23 @@ import { Routes, Route } from 'react-router-dom';
 // import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
+import WelcomePage from './pages/WelcomePage';
+import QRPage from './pages/QRPage';
+import ProfilePage from './pages/Profile';
+import SettingsPage from './pages/Settings';
 
 
 function App() {
   return (
     <div className="App">
-      <Routes> {/* The Switch decides which component to show based on the current URL.*/}
+      <Routes>
         <Route path='/' element={<LoginPage/>}></Route>
         <Route path='/login' element={<LoginPage/>}></Route>
         <Route path='/signup' element={<SignUpPage/>}></Route>
+        <Route path='/welcome' element={<WelcomePage/>}></Route>
+        <Route path='/qr' element={<QRPage/>}></Route>
+        <Route path='/profile' element={<ProfilePage/>}></Route>
+        <Route path='/settings' element={<SettingsPage/>}></Route>
         {/* <Route path='/home' element={<HomePage/>}></Route> */}
       </Routes>
     </div>
--- frontend/src/pages/WelcomePage.js
+++ frontend/src/pages/WelcomePage.js
@@ -1,21 +1,9 @@
 import "./css/WelcomePage.css";
 import { useNavigate } from "react-router-dom";
 
-import NavBar from "../components/NavBar";
-import TitleBar from "../components/TitleBar";
-
-
 
 function WelcomePage(){
   let navigate = useNavigate();
-
-  // TODO: check if any of the heart icons are overlapping with the text and if so
-  // TODO: then make the heart icon not visible
-
-  const navbar = <NavBar isHome={false} isQR={false} isProfile={false} />
-  const titlebar = <TitleBar title={"Heartbeat"} enableBack={true} />
-
-
 
   return (
     <div className='welcome-page'>
