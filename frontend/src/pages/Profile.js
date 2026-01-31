3edd0683afca645dd0fbbd3fb259cff03c6211bd
AidenComiskey <aiden.whisk@hotmail.com>
2026-01-31 18:25:50 +0000

    added titlebar and navbar to profile page

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,13 +1,17 @@
 import { Component } from "react";
 import "./Profile.css";
+import { useNavigate } from "react-router-dom";
+
+import NavBar from "../components/NavBar";
+import TitleBar from "../components/TitleBar";
 
 
-
-class ProfilePage extends Component{
-    render() {
+function ProfilePage() {
+    
         // these are just placeholders to be changed depending on users profile pic and name
         const username = "User Name";
         const profileImage = "Image.jpg";
+        const navigate = useNavigate();
 
         return (
         <div className="profile">
@@ -19,14 +23,16 @@ class ProfilePage extends Component{
                 <span>------ Options ------</span>
             </div>
             <div className="profile-options">
-                <button className="option-button">Settings</button>
+                <button className="option-button" onClick={() => navigate("/settings")}>Settings</button>
                 <button className="option-button">Scan User Code</button>
                 <button className="option-button">Contact GP</button>
                 <button className="option-button logout-button">Logout</button>
             </div>
+            <NavBar isHome={false} isQr={false} isProfile={true} />
+            <TitleBar title = "Heartbeat" enableBack={true}/>
         </div>
         );
-    }
+    
 }
 
 export default ProfilePage;
